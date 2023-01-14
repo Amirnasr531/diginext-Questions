5a7d45ea2490c3052c512b6fc7c10a4ea9a565f5
Amirnasr531 <121977269+Amirnasr531@users.noreply.github.com>
2023-01-14 23:13:47 +0330

    Add files via upload

---
+++ Question1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(function(a, b));
+            int function(string str, int n)
+            {
+                string[] text2 = new string[n];
+                char[] text = str.ToCharArray();
+                int k = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    text2[k] = Convert.ToString(text[k]);
+                    b++;
+                }
+                int s = 0;
+                for (int i = k; i < text2.Length; i++)
+                {
+                    text2[i] = Convert.ToString(text[s]);
+                    s++;
+                    if (s == str.Length)
+                    {
+                        s = 0;
+                    }
+                }
+                int counter = 0;
+                for (int i = 0; i < 1; i++)
+                {
+                    for (int l = 0; l < text2.Length; l++)
+                    {
+                        if (text2[i] == text2[l])
+                        {
+                            counter++;
+                        }
+                    }
+
+                }
+                return counter;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 1, 2, 3, 4, 5, 6, };
+            Console.WriteLine(function(numbers));
+            int function(int[] array)
+            {
+                int counter = 0;
+                int n = array.Length;
+                for (int j = 1; j < n; j++)
+                {
+                    int k = array[j];
+                    int i = j - 1;
+                    while ((i > -1) && (array[i] > k))
+                    {
+                        array[i + 1] = array[i];
+                        i--;
+                    }
+                    counter++;
+                    array[i + 1] = k;
+                }
+                return counter;
+            }
+            Console.ReadKey();
+        }
+    }
+}
